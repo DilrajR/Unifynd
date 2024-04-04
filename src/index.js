4973adc800fe19fd42d2be00ed4c19f2f7150e50
Dilraj Randhawa <dilraj_randhawa02@hotmail.com>
2024-04-04 01:19:35 -0400

    Removed Useless Files / Updated Index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Homepage from "./components/HomePage/Homepage";
 import AddItem from "./components/AddPostPage/AddItem";
-import AddUser from "./AddUser";
 import Login from "./components/LoginPage/Login";
 import Signup from "./components/SignupPage/Signup";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
@@ -22,7 +21,6 @@ root.render(
         <Route path="/" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
         <Route path="/AddItem" element={<AddItem />} />
-        <Route path="/AddUser" element={<AddUser />} />
         <Route path="/Home" element={<Homepage />} />
         <Route path="/Nav" element={<Navbar />} />
         <Route path="/Profile" element={<Profile />} />
